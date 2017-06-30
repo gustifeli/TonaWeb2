45fef71bfb7d60c7605a69b878f552dada48792b
gustifeli <gustifeli@hotmail.com>
2017-06-29 21:26:42 -0300

    scriptPrueba

---
+++ TonaWeb/scriptPrueba.sql
@@ -0,0 +1,5 @@
+﻿select * from Producto
+insert into Producto(descripcion, imagen, idCampana)
+values('descripcion', 'C:\Users\gusti\OneDrive\Documentos', 1);
+
+select * from  Producto p, Campana c where  c.idCampana = 1 
