9e47c7a9e9dfbdfb5dee4916635d014ea75b726b
gustifeli <gustifeli@hotmail.com>
2017-06-29 21:26:02 -0300

    cambios varios

---
+++ TonaWeb/ScriptBDTonaWeb.sql
@@ -0,0 +1,14 @@
+﻿create table Campana(
+idCampana int identity(1,1) not null,
+descripcion varchar(50)
+constraint pk_camp primary key(idCampana)
+)
+
+create table Producto(
+idProducto int identity(1,1) not null,
+descripcion varchar(100),
+imagen image not null,
+idCampana int,
+Constraint pk_prod primary key(idProducto),
+Constraint fk_prod_Camp foreign key (idCampana) references Campana(idCampana)
+)
---
+++ TonaWeb/TonaWeb/Models/ServiceProducto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TonaWeb.Models
+{
+    public class ServiceProducto
+    {
+        public void CrearProducto(TonaWeb.Models.Producto producto)
+        {
+            var db = new BaseDatosDataContext();
+            db.Producto.InsertOnSubmit(new TonaWeb.Producto()
+            {
+
+                descripcion = producto.Descripcion,
+                imagen = producto.Imagen,
+                //idCampana = producto.IdCamp,
+            });
+            db.SubmitChanges();
+        }
+    }
+}
